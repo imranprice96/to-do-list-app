81e8025d78459ddaea01a304f11dd2dc2326d36e
Imran Price <imranprice96@gmail.com>
2022-09-25 00:42:37 +1200

    Change dropdown image path

--- src/modules/pages/pageController.js
+++ src/modules/pages/pageController.js
@@ -1,99 +1,103 @@
-import allPage from './allPage';
-import todayPage from './todayPage';
-import projectsPage from './projectsPage';
-import projectController from '../projectController';
-import '../../style.css';
-import storage from '../../storage';
+import allPage from "./allPage";
+import todayPage from "./todayPage";
+import projectsPage from "./projectsPage";
+import projectController from "../projectController";
+import "../../style.css";
+import storage from "../../storage";
+import dropIcon from "../../icons/drop_down.png";
 
 const pageController = (() => {
-    const content = document.getElementById('content');
-    const all = document.getElementById('all');
-    const today = document.getElementById('today');
-    const projects = document.getElementById('projects');
-    const dropdown = document.getElementById('projects-dropdown');
-    let pageID = 'a';
-    
-    all.addEventListener('focus', (e) => {
-        loadAllPage();
-    });
-    today.addEventListener('focus', (e) => {
-        loadTodayPage();
-    });
+	const content = document.getElementById("content");
+	const all = document.getElementById("all");
+	const today = document.getElementById("today");
+	const projects = document.getElementById("projects");
+	const dropdown = document.getElementById("projects-dropdown");
+	let pageID = "a";
 
-   const loadAllPage = () =>{
-        content.innerHTML = '';
-        content.appendChild(allPage());
-        pageID = 'a';
-   };
+	all.addEventListener("focus", (e) => {
+		loadAllPage();
+	});
+	today.addEventListener("focus", (e) => {
+		loadTodayPage();
+	});
 
-   const loadTodayPage = () =>{
-    content.innerHTML = '';
-    content.appendChild(todayPage());
-    pageID = 't';
-   };
+	const loadAllPage = () => {
+		content.innerHTML = "";
+		content.appendChild(allPage());
+		pageID = "a";
+	};
 
-   const loadProjectPage = (project) => {
-        content.innerHTML = '';
-        content.appendChild(projectsPage(project));
-        pageID = [project.title];
-   };
+	const loadTodayPage = () => {
+		content.innerHTML = "";
+		content.appendChild(todayPage());
+		pageID = "t";
+	};
 
-   const projectDrop = () => {
-     projects.addEventListener('click', (e) => {
-        if(dropdown.style.display == 'flex'){
-            dropdown.style.display = 'none';
-        } else{
-            dropdown.style.display = 'flex';
-        }
-     });
+	const loadProjectPage = (project) => {
+		content.innerHTML = "";
+		content.appendChild(projectsPage(project));
+		pageID = [project.title];
+	};
 
-   }
+	const projectDrop = () => {
+		projects.addEventListener("click", (e) => {
+			if (dropdown.style.display == "flex") {
+				dropdown.style.display = "none";
+			} else {
+				dropdown.style.display = "flex";
+			}
+		});
+	};
 
-    const loadPage = () => {
-        content.innerHTML = '';
-        if(pageID == 'a'){
-            loadAllPage();
-        }else if(pageID == 't'){
-            loadTodayPage();
-        }else{
-            for(const p in projectController.projects){
-                if(projectController.projects[p].title == pageID){
-                    loadProjectPage(projectController.projects[p]);
-                }
-            }
-        };
-    };
+	const loadPage = () => {
+		content.innerHTML = "";
+		if (pageID == "a") {
+			loadAllPage();
+		} else if (pageID == "t") {
+			loadTodayPage();
+		} else {
+			for (const p in projectController.projects) {
+				if (projectController.projects[p].title == pageID) {
+					loadProjectPage(projectController.projects[p]);
+				}
+			}
+		}
+	};
 
-    const loadNav = () => {
-        dropdown.innerHTML = '';
-        for(const p in projectController.projects){
-            if(projectController.projects[p].title != 'default'){
-                const div = document.createElement('div');
-                div.innerHTML = projectController.projects[p].title;
-                div.setAttribute('tabindex', 0);
-                div.classList.add('project-nav-tem');
-                dropdown.appendChild(div);
+	const loadNav = () => {
+		dropdown.innerHTML = "";
+		const dropImage = new Image();
+		dropImage.src = dropIcon;
+		document.getElementById("drop-down").appendChild(dropImage);
 
-                div.addEventListener('focus', (e) => {
-                    loadProjectPage(projectController.projects[p]);
-                });
-            }
-        }
-    };
+		for (const p in projectController.projects) {
+			if (projectController.projects[p].title != "default") {
+				const div = document.createElement("div");
+				div.innerHTML = projectController.projects[p].title;
+				div.setAttribute("tabindex", 0);
+				div.classList.add("project-nav-tem");
+				dropdown.appendChild(div);
 
-    const reload = () =>{
-        loadPage();
-        loadNav();
-        storage.saveData();
-    }
+				div.addEventListener("focus", (e) => {
+					loadProjectPage(projectController.projects[p]);
+				});
+			}
+		}
+	};
 
-    return {
-        loadPage,
-        loadNav,
-        reload,
-        projectDrop,
-        loadAllPage,
-    };
+	const reload = () => {
+		loadPage();
+		loadNav();
+		storage.saveData();
+	};
+
+	return {
+		loadPage,
+		loadNav,
+		reload,
+		projectDrop,
+		loadAllPage,
+	};
 })();
 
 export default pageController;
